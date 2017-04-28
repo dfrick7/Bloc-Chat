e8903b63aaa2f486a36100d22602603c92c90209
Daniel Frick <danielfrick7@gmail.com>
2017-04-27 20:37:01 -0500

    some styling

--- app/scripts/services/Messages.js
+++ app/scripts/services/Messages.js
@@ -1,7 +1,11 @@
 (function() {
   function Message($firebaseArray) {
+    var Message = {};
+
     var ref = firebase.database().ref().child('messages');
     var messages = $firebaseArray(ref);
+
+
     return {
       getByRoomId: function (roomId) {
         console.log(roomId);
